50b6cfb0f7c6bff31fe61f0fcd9b985f74b98783
TheOMUU <omkarravindrarathod@gmail.com>
2026-08-11 17:27:32 +0530

    Finding second smallest element in the array

---
+++ 01-Arrays/SecondSmallestElement.java
@@ -0,0 +1,21 @@
+public class SecondSmallestElement {
+    public static void main(String[] args) {
+        //Initialize the array
+        int[] num = {8,4,2,5,44,33,2,-2};
+        secondSmallest(num);
+    }
+    //Method for displaying Second Smallest Element in the array
+    public static void secondSmallest(int[] arr){
+        int min = arr[0];
+        int secondMin = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(min >arr[i]){
+                secondMin = min;
+                min = arr[i];
+            } else if (arr[i] < min && arr[i] != min) {
+                secondMin = arr[i];
+            }
+        }
+        System.out.println("Second Smallest Element in the array is " + secondMin);
+    }
+}
